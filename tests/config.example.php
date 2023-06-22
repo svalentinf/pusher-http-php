a940c8467dfe09de19d26dfc4532aaa1cfc9f20b
valentin <valentin>
2023-06-22 18:20:51 +0300

    downgrade to 7.0.1

--- tests/config.example.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-const PUSHERAPP_APPID = '';
-const PUSHERAPP_AUTHKEY = '';
-const PUSHERAPP_SECRET = '';
-const PUSHERAPP_CLUSTER = 'eu';
-const TEST_CHANNEL = 'my-channel';
-const CHANNEL_PREFIX = 'my-';
